9228483a05a9bb8d04f3f88b5834f4c6533de053
Noemi Pusceddu <m_emi94@hotmail.it>
2023-09-11 16:48:01 +0200

    first commit

---
+++ D6.js
@@ -0,0 +1,293 @@
+/* ESERCIZIO 1
+  Scrivi una funzione per concatenare due stringhe ricevute come parametri, selezionando solamente i primi 2 caratteri della
+  prima e gli ultimi 3 della seconda. Converti la stringa risultante in maiuscolo e mostrala con un console.log().
+*/
+
+// const str1 = 'Ciao '
+// const str2 = 'mondo'
+
+// const stringa = function (s1, s2) {
+//   return s1.slice(0, 2).concat(s2.slice(s2.length - 3, s2.length))
+// }
+// const nuovaStringa = stringa(str1, str2).toUpperCase()
+
+// console.log(nuovaStringa)
+
+/* ESERCIZIO 2 (for)
+  Scrivi una funzione che torni un array di 10 elementi; ognuno di essi deve essere un valore random compreso tra 0 e 100 (incluso).
+*/
+
+// const random1 = function () {
+//   const r = []
+//   for (let i = 0; i < 10; i++) {
+//     r.push(Math.floor(Math.random() * 100) + 1)
+//   }
+//   return r
+// }
+
+// console.log(random1())
+
+/* ESERCIZIO 3 (filter)
+  Scrivi una funzione per ricavare solamente i valori PARI da un array composto da soli valori numerici
+*/
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const pari = arr.filter((n) => {
+//   return n % 2 === 0
+// })
+
+// console.log(pari)
+
+/* ESERCIZIO 4 (forEach)
+  Scrivi una funzione per sommare i numeri contenuti in un array
+*/
+// const arr1 = [7, 14, 21, 28]
+// let total = 0
+// arr1.forEach(function (number) {
+//   total += number
+// })
+// console.log(total)
+
+/* ESERCIZIO 5 (reduce)
+  Scrivi una funzione per sommare i numeri contenuti in un array
+*/
+
+// const arr2 = [5, 10, 15, 20]
+// const result2 = arr2.reduce((acc, n) => {
+//   return acc + n
+// })
+
+// console.log(result2)
+
+/* ESERCIZIO 6 (map)
+  Scrivi una funzione che, dato un array di soli numeri e un numero n come parametri, ritorni un secondo array con tutti i valori del precedente incrementati di n
+*/
+
+// const incrementOne = function (arr3, n) {
+//   const result = arr3.map((val) => val + n)
+//   return result
+// }
+
+// console.log(incrementOne([5, 28, 98, 108], 3))
+
+/* ESERCIZIO 7 (map)
+  Scrivi una funzione che, dato un array di stringhe, ritorni un nuovo array contenente le lunghezze delle rispettive stringhe dell'array di partenza
+  es.: ["EPICODE", "is", "great"] => [7, 2, 5]
+*/
+
+// const arrayStringhe = ['farfalla', 'cane', 'gatto', 'delfino']
+
+// const nuovoArrayStringhe = arrayStringhe.map((index) => {
+//   return index.length
+// })
+
+// console.log(nuovoArrayStringhe)
+
+/* ESERCIZIO 8 (forEach o for)
+  Scrivi una funzione per creare un array contenente tutti i valori DISPARI da 1 a 99.
+*/
+
+// const hundred = Math.floor(Math.random() * 100) + 1
+
+// const dispari = []
+
+// hundred.forEach((n) => {
+//   if (n % 2 !== 0) {
+//     dispari.push(n)
+//   }
+// })
+
+// console.log(dispari)
+
+/* Questo array di film verrà usato negli esercizi a seguire. Non modificarlo e scorri oltre per riprendere gli esercizi :) */
+const movies = [
+  {
+    Title: 'The Lord of the Rings: The Fellowship of the Ring',
+    Year: '2001',
+    imdbID: 'tt0120737',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lord of the Rings: The Return of the King',
+    Year: '2003',
+    imdbID: 'tt0167260',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lord of the Rings: The Two Towers',
+    Year: '2002',
+    imdbID: 'tt0167261',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lord of War',
+    Year: '2005',
+    imdbID: 'tt0399295',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lords of Dogtown',
+    Year: '2005',
+    imdbID: 'tt0355702',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lord of the Rings',
+    Year: '1978',
+    imdbID: 'tt0077869',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lord of the Flies',
+    Year: '1990',
+    imdbID: 'tt0100054',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lords of Salem',
+    Year: '2012',
+    imdbID: 'tt1731697',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Greystoke: The Legend of Tarzan, Lord of the Apes',
+    Year: '1984',
+    imdbID: 'tt0087365',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lord of the Flies',
+    Year: '1963',
+    imdbID: 'tt0057261',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Avengers',
+    Year: '2012',
+    imdbID: 'tt0848228',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Avengers: Infinity War',
+    Year: '2018',
+    imdbID: 'tt4154756',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Avengers: Age of Ultron',
+    Year: '2015',
+    imdbID: 'tt2395427',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Avengers: Endgame',
+    Year: '2019',
+    imdbID: 'tt4154796',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg',
+  },
+]
+
+/* ESERCIZIO 9 (forEach)
+  Scrivi una funzione per trovare il film più vecchio nell'array fornito.
+*/
+
+// const older = function () {
+//   return movies(Math.min)
+// }
+
+// console.log(older)
+
+/* ESERCIZIO 10
+  Scrivi una funzione per ottenere il numero di film contenuti nell'array fornito.
+*/
+
+// const numberOfMovies = function () {
+//   return movies.length()
+// }
+
+// console.log(numberOfMovies)
+
+/* ESERCIZIO 11 (map)
+  Scrivi una funzione per creare un array con solamente i titoli dei film contenuti nell'array fornito.
+*/
+// const onlyTitles = []
+// const titles = function () {
+//   onlyTitles = movies.map()
+//   return onlyTitles
+// }
+
+// console.log(titles)
+
+/* ESERCIZIO 12 (filter)
+  Scrivi una funzione per ottenere dall'array fornito solamente i film usciti nel millennio corrente.
+*/
+
+// const filteredMovies = movies.filter((Year) => {
+//   return Year >= 2000
+// })
+
+// console.log(filteredMovies)
+
+/* ESERCIZIO 13 (reduce)
+  Scrivi una funzione per calcolare la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array fornito.
+*/
+
+// const sumOfYears = movies.reduce((acc, n) => {
+//   return acc + n
+// })
+
+// console.log(sumOfYears)
+
+/* ESERCIZIO 14 (find)
+  Scrivi una funzione per ottenere dall'array fornito uno specifico film (la funzione riceve un imdbID come parametro).
+*/
+
+// const movie = movies.find(function (imdbID) {
+//   return movies.slice(3, 1) === 1
+// })
+
+// console.log(movie)
+
+/* ESERCIZIO 15 (findIndex)
+  Scrivi una funzione per ottenere dall'array fornito l'indice del primo film uscito nell'anno fornito come parametro.
+*/
+
+// const firstMovie = function () {
+//   const first = movies.findIndex(Year, '2003')
+//   return first
+// }
+
+// const firstMovie = movies.findIndex((Year) => {
+//   return movies.Title
+// })
+
+// console.log(firstMovie('2003'))
